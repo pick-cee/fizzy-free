fa57801424ad6a20aad9341eb6103e735747e77b
pick-cee <akinloluwaolumuyide@gmail.com>
2025-06-11 15:36:01 +0100

    feat: update service worker to import Workbox functions and manage caching

--- src/sw.ts
+++ src/sw.ts
@@ -1,16 +1,21 @@
-// This is our new custom service worker file
-
 /// <reference lib="webworker" />
 declare const self: ServiceWorkerGlobalScope;
 
-// This event listener is required by the PWA plugin to manage caching
+// FIX: Import the necessary Workbox functions
+import { precacheAndRoute } from 'workbox-precaching';
+
+// This is the placeholder the build error is asking for.
+// The PWA plugin will replace `self.__WB_MANIFEST` with a list of all your app's files (JS, CSS, etc.) to cache.
+precacheAndRoute(self.__WB_MANIFEST);
+
+// This event listener is required by the PWA plugin to manage updates.
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
 
-// Our custom notification click handler
+// Our custom notification click handler.
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
   event.waitUntil(
